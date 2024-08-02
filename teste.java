b47b022d44fdd8ca0932d1c472ebd67d75db866a
Taina <suareztaina47@gmail.com>
2024-08-02 20:23:58 -0300

    novos prints

--- teste.java
+++ teste.java
@@ -1,5 +1,8 @@
 public class teste{
     public static void main(String[] args){
-        System.out.println("Ola mundo!");
+        System.out.println("123123!");
+
+        System.out.println("123123!");
+
     }
 }
